085a1947c224e4b70387dfa38590d201c71b861d
Kasia <pernalk@gmail.com>
2021-03-11 17:30:43 +0100

    SAPT tests added to gammcor_verify.py

--- gammcor_verify.py
+++ gammcor_verify.py
@@ -1,81 +1,153 @@
 #! /usr/bin/env python
-import os,sys,string,math,numpy
+import os,sys,string,math
+import numpy as np
+import subprocess
 from numpy.testing import assert_allclose
 from numpy.testing import assert_approx_equal
 
+## user defined 
+RUN_GAMMCOR = "/home/kasia/GAMMCOR/gammcor"
+
 ### functions reading components of SAPT
-def elst(file):
-        f=open(file,'r')
-        s=f.read()
-        f.close()
-        a=s.find(" E1elst      =")
-        S=s[a+18:a+31]
-        return float(S)
+## SAPT ENERGY TAGS
+ELST_TAG = "E1elst"
+E1EX_TAG = "E1exch(S2)"
+IND_TAG  = "E2ind "
+DSP_TAG  = "E2disp "
+DCS_TAG  = "E2disp(CAS)"
+DRV_TAG  = "E2disp(SCALED)  "
+WI1_TAG  = "Wind_12"
+WI2_TAG  = "Wind_13"
+WD1_TAG  = "Wdisp_12"
+WD2_TAG  = "Wdisp_13"
+EXI_TAG  = "E2exch-ind "
+EXD_TAG  = "E2exch-disp"
+XDRV_TAG = "E2exdisp(SCALED)"
+EINT_TAG = "Eint(SAPT2)"
+SLVL_TAG = "SAPT level"
+
+IND0_TAG = "E2ind(unc)"
+DSP0_TAG = "E2disp(unc)"
+SP_TAG   = "E2disp(sp)"
+SC_TAG   = "E2disp(sc)"
+EXD0_TAG = "E2exch-disp(unc)"
+EXI0_TAG = "E2exch-ind(unc)"
+INT0_TAG = "Eint(SAPT0)"
 #
-def exch(file):
-        f=open(file,'r')
-        s=f.read()
-        f.close()
-        a=s.find(" E1exch(S2)  =")
-        S=s[a+18:a+31]
-        return float(S)
+def get_jobname(log_path,log_name):
+   output = os.path.join(log_path,log_name)
+   f = open(output)
+   for line in f:
+      line_adjl = line.lstrip()
+      if line_adjl.startswith("JobTitle"):
+         job_name = line_adjl.replace("JobTitle","").lstrip()
+
+   return job_name
 #
-def ind(file):
-        f=open(file,'r')
-        s=f.read()
-        f.close()
-        a=s.find(" E2ind       =")
-        S=s[a+18:a+31]
-        return float(S)
+def get_sapt2_energies(log_path,log_name):
+    output = os.path.join(log_path,log_name)
+    e_elst = 0.0
+    e_exch = 0.0
+    e_ind  = 0.0
+    e_exi  = 0.0
+    e_disp = 0.0
+    e_exd  = 0.0
+    e_int  = 0.0
+    wd_12  = 0.0
+    wd_13  = 0.0
+    f = open(output)
+    SAPT2 = []
+    for line in f:
+
+        if SLVL_TAG in line:
+           for line in f:
+               line_adjl = line.lstrip("! ")
+               if line_adjl.startswith(ELST_TAG):
+                   e_elst = float(line_adjl.split("=")[1])
+                   SAPT2.append([ELST_TAG,e_elst])
+               if line_adjl.startswith(E1EX_TAG):
+                   e_exch = float(line_adjl.split("=")[1])
+                   SAPT2.append([E1EX_TAG,e_exch])
+               if line_adjl.startswith(IND_TAG):
+                   e_ind  = float(line_adjl.split("=")[1])
+                   SAPT2.append([IND_TAG,e_ind])
+               if line_adjl.startswith(EXI_TAG):
+                   e_exi  = float(line_adjl.split("=")[1])
+                   SAPT2.append([EXI_TAG,e_exi])
+               if line_adjl.startswith(DSP_TAG):
+                   e_disp = float(line_adjl.split("=")[1])
+                   SAPT2.append([DSP_TAG,e_disp])
+               if line_adjl.startswith(DCS_TAG):
+                   e_disp = float(line_adjl.split("=")[1])
+                   SAPT2.append([DCS_TAG,e_disp])
+               if line_adjl.startswith(DRV_TAG):
+                   e_disp = float(line_adjl.split("=")[1])
+                   SAPT2.append([DRV_TAG,e_disp])
+               if line_adjl.startswith(EXD_TAG):
+                   e_exd  = float(line_adjl.split("=")[1])
+                   SAPT2.append([EXD_TAG,e_exd])
+               if line_adjl.startswith(XDRV_TAG):
+                   e_exd  = float(line_adjl.split("=")[1])
+                   SAPT2.append([XDRV_TAG,e_exd])
+               if line_adjl.startswith(EINT_TAG):
+                   e_int  = float(line_adjl.split("=")[1])
+                   SAPT2.append([EINT_TAG,e_int])
+               if line_adjl.startswith(WD1_TAG):
+                   wd_12  = float(line_adjl.split("=")[1])
+                   SAPT2.append([WD1_TAG,wd_12])
+               if line_adjl.startswith(WD2_TAG):
+                   wd_13  = float(line_adjl.split("=")[1])
+                   SAPT2.append([WD2_TAG,wd_13])
+
+    return SAPT2
 #
-def indBA(file):
-        f=open(file,'r')
-        s=f.read()
-        f.close()
-        a=s.find(" Ind(B<--A)     =")
-        S=s[a+19:a+35]
-        return float(S)
-#
-def indAB(file):
-        f=open(file,'r')
-        s=f.read()
-        f.close()
-        a=s.find(" Ind(A<--B)     =")
-        S=s[a+19:a+35]
-        return float(S)
-#
-def exch_ind(file):
-        f=open(file,'r')
-        s=f.read()
-        f.close()
-        a=s.find(" E2exch-ind  =")
-        S=s[a+18:a+31]
-        return float(S)
-#
-def disp(file):
-        f=open(file,'r')
-        s=f.read()
-        f.close()
-        a=s.find(" E2disp      =")
-        S=s[a+18:a+31]
-        return float(S)
-#
-def exch_disp(file):
-        f=open(file,'r')
-        s=f.read()
-        f.close()
-        a=s.find(" E2exch-disp =")
-        S=s[a+18:a+31]
-        return float(S)
-#
+def get_sapt0_energies(log_path,log_name):
+    output = os.path.join(log_path,log_name)
+    e_elst = 0.0
+    e_exch = 0.0
+    e_ind  = 0.0
+    e_exi  = 0.0
+    e_disp = 0.0
+    e_exd  = 0.0
+    f = open(output)
+    SAPT0 = []
+    for line in f:
+
+        if SLVL_TAG in line:
+           for line in f:
+            line_adjl = line.lstrip("! ")
+            if line_adjl.startswith(ELST_TAG):
+                e_elst = float(line_adjl.split("=")[1])
+                SAPT0.append([ELST_TAG,e_elst])
+            if line_adjl.startswith(E1EX_TAG):
+                e_exch = float(line_adjl.split("=")[1])
+                SAPT0.append([E1EX_TAG,e_exch])
+            if line_adjl.startswith(IND0_TAG):
+                e_ind  = float(line_adjl.split("=")[1])
+                SAPT0.append([IND0_TAG,e_ind])
+            if line_adjl.startswith(EXI0_TAG):
+                e_exi  = float(line_adjl.split("=")[1])
+                SAPT0.append([EXI0_TAG,e_exi])
+            if line_adjl.startswith(DSP0_TAG):
+                e_disp  = float(line_adjl.split("=")[1])
+                SAPT0.append([DSP0_TAG,e_disp])
+            if line_adjl.startswith(EXD0_TAG):
+                e_exd  = float(line_adjl.split("=")[1])
+                SAPT0.append([EXD0_TAG,e_exd])
+
+    f.close()
+    e_int = e_elst + e_exch + e_ind + e_exi + e_disp + e_exd
+    SAPT0.append([INT0_TAG,e_int])
+
+    return SAPT0
 #
 def ac0_en(file):
-	f=open(file,'r')
-        s=f.read()
-        f.close()
-        a=s.find(" ECASSCF+ENuc, AC0-Corr, AC0-CASSCF")
-        S=s[a+70:a+83]
-        return float(S)
+   f=open(file,'r')
+   s=f.read()
+   f.close()
+   a=s.find(" ECASSCF+ENuc, AC0-Corr, AC0-CASSCF")
+   S=s[a+70:a+83]
+   return float(S)
 #
 def ac_en(file):
         f=open(file,'r')
@@ -118,14 +190,14 @@ def acgvb_en(file):
         return float(S)
 #
 def ac1gvb_en(file):
-        f=open(file,'r')
-	for line in f:
-		if line[0:26] == " EGVB+ENuc, Corr, ERPA-GVB" :
-			l=line.split(" ")
-			le=len(l)-1
-			l=l[le]
-			S=l[0:len(l)-1]
-        return float(S)
+   f=open(file,'r')
+   for line in f:
+      if line[0:26] == " EGVB+ENuc, Corr, ERPA-GVB" :
+         l=line.split(" ")
+         le=len(l)-1
+         l=l[le]
+         S=l[0:len(l)-1]
+   return float(S)
 #
 def eerpagvb_en(file):
         f=open(file,'r')
@@ -148,14 +220,16 @@ def caspidft_en(file):
 #
 def run_single(dir,fun):
 # run calc in dir and extract data with fun
-	cwd=os.getcwd()
-	wrkdir=cwd+"/"+dir
-        os.chdir(wrkdir)
-	os.system(cwd+"/gammcor > "+cwd+"/test.out")
-	os.chdir(cwd)
-	t=fun("test.out")
-	os.system("rm test.out")
-	return t
+   cwd=os.getcwd()
+   wrkdir=cwd+"/"+dir
+   os.chdir(wrkdir)
+  # os.system(cwd+"/gammcor > "+cwd+"/test.out")
+   cmd = "{execpath} > {output}".format(execpath=RUN_GAMMCOR, output=cwd+"/test.out")
+   subprocess.call(cmd, shell=True)
+   os.chdir(cwd)
+   t=fun("test.out")
+   os.system("rm test.out")
+   return t
 #
 def reference(dirl,fun):
         ref_val=[]
@@ -165,72 +239,71 @@ def reference(dirl,fun):
 #
 def run_test(dirl,fun):
 	ref_val=reference(dirl,fun)
-	print 'ref :',ref_val
+	print('ref :',ref_val)
 #
 	test_val=[]
 	for dir in dirl:
 		test_val.append(run_single(dir,fun))
 #	
-	print 'test:',test_val
+	print('test:',test_val)
 	try:
 		assert_allclose(test_val,ref_val,rtol=0,atol=tolene)
-		print 'passed with tol=',tolene,'[Ha] \n'
+		print('passed with tol=',tolene,'[Ha] \n')
 	except:
-		print ' failed with errors:'
-		print numpy.subtract(test_val,ref_val)	
+		print(' failed with errors:')
+		print(np.subtract(test_val,ref_val))
 	return 
 #
-def run_test_sapt(dirl):
+def run_test_sapt(dirl,slevel):
 #
-	tolsapt=1.e-5
-	ref_val=[]
-        test_val=[]
-	cwd=os.getcwd()
-        for dir in dirl:
-		wrkdir=cwd+"/"+dir
-		os.chdir(wrkdir)
-		os.system(cwd+"/gammcor > "+cwd+"/test.out")
-		os.chdir(cwd)
-# elst
-                ref_val.append(elst(dir+"/gammcor.out"))
-                test_val.append(elst("test.out"))
-# exch
-                ref_val.append(exch(dir+"/gammcor.out"))
-                test_val.append(exch("test.out"))
-# ind
-                ref_val.append(ind(dir+"/gammcor.out"))
-                test_val.append(ind("test.out"))
-# indBA
-                ref_val.append(indBA(dir+"/gammcor.out"))
-                test_val.append(indBA("test.out"))
-# indAB
-                ref_val.append(indAB(dir+"/gammcor.out"))
-                test_val.append(indAB("test.out"))
-# exch_ind
-                ref_val.append(exch_ind(dir+"/gammcor.out"))
-                test_val.append(exch_ind("test.out"))
-# disp
-		ref_val.append(disp(dir+"/gammcor.out"))
-		test_val.append(disp("test.out"))	
-# exch_disp
-		ref_val.append(exch_disp(dir+"/gammcor.out"))
-                test_val.append(exch_disp("test.out"))   
-#       
-	print 'ref :',ref_val
-        print 'test:',test_val
-#
-        try:
-                assert_allclose(test_val,ref_val,rtol=0,atol=tolsapt)
-                print 'passed with tol=',tolsapt,'[mHa] \n'
-        except:
-                print ' failed with errors:'
-                print numpy.subtract(test_val,ref_val)
-        return
+   tolsapt=1.e-5
+   ref_val =[]
+   test_val=[]
+   cwd=os.getcwd()
+   for dir in dirl:
+      wrkdir=cwd+"/"+dir
+      os.chdir(wrkdir)
+      cmd = "{execpath} > {output}".format(execpath=RUN_GAMMCOR, output=wrkdir+"/test.out")
+      subprocess.call(cmd, shell=True)
+      os.chdir(cwd)
+      if slevel=='sapt2' :
+         ref_tab = get_sapt2_energies(dir,"gammcor.out")
+         tst_tab = get_sapt2_energies(dir,"test.out")
+      elif slevel=='sapt0' :
+         ref_tab = get_sapt0_energies(dir,"gammcor.out")
+         tst_tab = get_sapt0_energies(dir,"test.out")
+
+
+      ref_lbl = [ref_tab[i][0] for i in range(len(ref_tab))]
+      ref_val = [ref_tab[i][1] for i in range(len(ref_tab))]
+      tst_lbl = [tst_tab[i][0] for i in range(len(tst_tab))]
+      tst_val = [tst_tab[i][1] for i in range(len(tst_tab))]
+
+      jname = get_jobname(dir,"input.inp")
+      print("Job name: ",jname)
+      print("{ECONT:>16} {CALC:>12} {REF:>12} {DIFF:>12} {IPASS}".format(ECONT="Energy",
+            CALC="ref_val",REF="test_val",DIFF="ref-tst",IPASS="Check "))
+      resid = np.zeros(len(ref_val))
+      for i in range(len(ref_val)):
+         resid[i] = abs(tst_val[i] - ref_val[i])
+         print("{ECONT}  {CALC:>12}  {REF:>12}  {DIFF}  {IPASS}".format(ECONT="%16s"%ref_lbl[i], 
+               CALC="%11.8f"%tst_val[i], REF="%11.8f"%ref_val[i], DIFF="%10.8f"%resid[i],IPASS=resid[i]<=tolsapt))
+      try:
+         assert_allclose(tst_val,ref_val,rtol=0,atol=tolsapt)
+         print('passed with tol=',tolsapt,'[mHa] \n')
+      except:
+         print(' failed with errors:')
+         print(np.subtract(tst_val,ref_val))
+
+   return
 #
 ###### BEGIN TESTS ######
-## SAPT(CAS) ##
-dir_sapt=["TESTS/SAPT/TEST1"]
+
+subprocess.call("export MKL_NUM_THREADS=1", shell=True)
 #
+## SAPT ##
+dir_sapt2=["TESTS/SAPT/TEST1","TESTS/SAPT/TEST2","TESTS/SAPT/TEST3","TESTS/SAPT/TEST4","TESTS/SAPT/TEST5","TESTS/SAPT/TEST6","TESTS/SAPT/TEST7","TESTS/SAPT/GVB"]
+dir_sapt0=["TESTS/SAPT/SAPT0"]
 #
 tolene=1.e-7
 ## AC0 ##
@@ -238,63 +311,68 @@ dir_ac0=["TESTS/AC0/TEST1","TESTS/AC0/TEST1/INCORE_INTEG" \
         ]
 #         ,"TESTS/AC0/TEST2", \
 #         "TESTS/AC0/TEST2/INCORE_INTEG"]
-## AC ##
+### AC ##
 dir_ac=["TESTS/AC/TEST1","TESTS/AC/TEST1/INCORE_INTEG"]
-## AC1 ##
+### AC1 ##
 dir_ac1=["TESTS/AC1/TEST1","TESTS/AC1/TEST1/INCORE_INTEG"]
-## AC0D ##
+### AC0D ##
 dir_ac0d=["TESTS/AC0D/TEST2"]
-## CASPIDFT ##
+### CASPIDFT ##
 dir_caspidft=["TESTS/CASPIDFT/TEST1","TESTS/CASPIDFT/TEST2"]
-## AC0-GVB ##
+### AC0-GVB ##
 dir_ac0gvb=["TESTS/AC0GVB/TEST1","TESTS/AC0GVB/TEST1/INCORE_INTEG"]
-## AC-GVB ##
+### AC-GVB ##
 dir_acgvb=["TESTS/ACGVB/TEST1","TESTS/ACGVB/TEST1/INCORE_INTEG"]
-## AC1-GVB ##
+### AC1-GVB ##
 dir_ac1gvb=["TESTS/AC1GVB/TEST1","TESTS/AC1GVB/TEST1/INCORE_INTEG"]
 ## EERPA-GVB ##
 dir_eerpagvb=["TESTS/EERPAGVB/TEST1","TESTS/EERPAGVB/TEST2","TESTS/EERPAGVB/TEST3","TESTS/EERPAGVB/TEST4"]
-## AC0_DALTON WITH CASSCF ##
+### AC0_DALTON WITH CASSCF ##
 dir_ac0dalton=["TESTS/AC0_DALTON/TEST1","TESTS/AC0_DALTON/TEST1/INCORE","TESTS/AC0_DALTON/TEST2","TESTS/AC0_DALTON/TEST2/INCORE"]
-## AC0_DALTON WITH HF WAVEFUNCTION ##
+### AC0_DALTON WITH HF WAVEFUNCTION ##
 dir_ac0_hf_dalton=["TESTS/AC0_HF_DALTON"]
 #
 # run tests
 #
-print "* testing SAPT energy components"
-run_test_sapt(dir_sapt)
+print ("* testing PIDFT energy")
+run_test(dir_caspidft,caspidft_en)
+sys.exit()
+
+print("* testing SAPT energy components \n")
+run_test_sapt(dir_sapt2,'sapt2')
+#run_test_sapt(dir_sapt0,'sapt0')
 #
-print "* testing EERPA-GVB energy"
+print("* testing EERPA-GVB energy")
 run_test(dir_eerpagvb,eerpagvb_en)
 #
-print "* testing AC0-GVB energy"
+print ("* testing AC0-GVB energy")
 run_test(dir_ac0gvb,ac0gvb_en)
 #
-print "* testing AC-GVB energy"
+print ("* testing AC-GVB energy")
 run_test(dir_acgvb,acgvb_en)
-#
-print "* testing AC1-GVB energy"
+##
+print ("* testing AC1-GVB energy")
 run_test(dir_ac1gvb,ac1gvb_en)
-#
-print "* testing AC0-CAS[molpro] energy"
+##
+print("* testing AC0-CAS[molpro] energy")
 run_test(dir_ac0,ac0_en)
-#
-print "* testing AC0-CAS[dalton] energy"
+##
+print ("* testing AC0-CAS[dalton] energy")
 run_test(dir_ac0dalton,ac0_en)
-#
-print "* testing AC0-HF[dalton] energy"
+##
+print ("* testing AC0-HF[dalton] energy")
 run_test(dir_ac0_hf_dalton,ac0_en)
-#
-print "* testing AC-CAS[molpro] energy"
+##
+print ("* testing AC-CAS[molpro] energy")
 run_test(dir_ac,ac_en)
-#
-print "* testing AC1-CAS[molpro] energy"
+##
+print ("* testing AC1-CAS[molpro] energy")
 run_test(dir_ac1,ac1_en)
-#
-print "* testing PIDFT energy"
+##
+print ("* testing PIDFT energy")
 run_test(dir_caspidft,caspidft_en)
-#
-print "* testing AC0D-CAS[molpro]"
+##
+print ("* testing AC0D-CAS[molpro]")
 run_test(dir_ac0d,ac0d_en)
 
 
