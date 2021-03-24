b2dba0644975ad6f26af9f8ee2a7f9709ab057e8
SokolAK <sokolak87@gmail.com>
2021-03-24 13:29:31 +0100

    test: add auto_test scripts

---
+++ auto_test/pre-push.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running pre-push hook"
+gammcor_verify.bash 'full'
+
+# $? stores exit value of the last command
+if [ $? -ne 0 ]; then
+ echo "Tests must pass before push!"
+ exit 1
+fi
