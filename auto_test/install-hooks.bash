9478d5bed5cfa66162daff582c23dfa8bcc0db02
SokolAK <sokolak87@gmail.com>
2021-04-13 14:26:22 +0200

    :bug: fix installation of post-merge hook

--- auto_test/install-hooks.bash
+++ auto_test/install-hooks.bash
@@ -6,5 +6,5 @@ echo "Installing hooks..."
 # this command creates symlink to our scripts
 ln -s ../../auto_test/pre-commit.bash $GIT_DIR/hooks/pre-commit
 ln -s ../../auto_test/pre-push.bash $GIT_DIR/hooks/pre-push
-ln -s ../../auto_test/pre-push.bash $GIT_DIR/hooks/post-merge
+ln -s ../../auto_test/post-merge.bash $GIT_DIR/hooks/post-merge
 echo "Done!"
