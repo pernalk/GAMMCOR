c14dcb62255c6e4c1466f4c7c64ac853ac8207dd
ola@andor <aleksandra.tucholska@gmail.com>
2023-03-30 12:19:08 +0200

    1. dodane testjobs: ACn, SAPT(Dalton,QPackage) 2. dodane -fallow-argument-mismatch w GNUConfig.cmake

---
+++ testjobs/run_tests.py
@@ -0,0 +1,87 @@
+#! /usr/bin/env python
+import os,sys,string,math
+from pathlib import Path
+import numpy as np
+from scripts import test_list, test_utils
+from scripts.colors import *
+
+########################################################################
+# PREPARE TESTS
+########################################################################
+
+### Enter to test directory
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__)) + "/"
+os.chdir(SCRIPT_PATH)
+
+### Set executable path
+if len(sys.argv) < 2:
+    raise Exception("Executable path not specified!")
+
+local_path_gammcor = sys.argv[1]
+
+if "gammcor" in local_path_gammcor:
+    local_path_gammcor = local_path_gammcor.split("gammcor")[0]
+
+if local_path_gammcor.endswith("/"):
+    local_path_gammcor += 'gammcor/build/'
+else:
+    local_path_gammcor += '/gammcor/build/'
+
+RUN_GAMMCOR = local_path_gammcor + 'gammcor'
+
+if not Path(RUN_GAMMCOR).is_file():
+    raise Exception(f"{RUN_GAMMCOR} not found!")
+### Set test level
+TEST_LEVEL = 'short'
+#
+# other options:
+# long
+# full
+#
+if len(sys.argv) > 2:
+    TEST_LEVEL = sys.argv[2]
+print(f"{OKCYAN}TEST LEVEL: {TEST_LEVEL}{ENDC}")
+
+
+########################################################################
+# RUN TESTS
+########################################################################
+no_passed = no_failed = no_total = 0
+tests_failed = []
+
+for test in test_list.get():
+    units = test_utils.prepare_test(test, TEST_LEVEL)
+    if len(units) > 0:
+        result = test_utils.start_test(test, units, RUN_GAMMCOR)
+        no_total += result.get('passed') + result.get('failed')
+        no_passed += result.get('passed')
+        no_failed += result.get('failed')
+        tests_failed += result.get('failed_tests')
+        # if result.get('failed'):
+        #     print(f"{FAIL}FAILED{ENDC}")
+        # else:
+        #     print(f"{OKGREEN}PASSED{ENDC}")
+        # print("\n---------------------------------------------\n")
+
+########################################################################
+# SUMMARY
+########################################################################
+if no_failed == 0:
+    print(OKGREEN)
+else:
+    print(FAIL)
+
+print("===============================================")
+print(f"\n{no_passed} / {no_failed} / {no_total} (passed / failed / total)\n")
+
+if no_failed == 0:
+    print(f"*** SUCCESS ***")
+    print(f"\n===============================================")
+    exit(0)
+else:
+    print(f"*** FAILURE ***")
+    print("\nFailed tests:")
+    for name in tests_failed:
+        print(f"* {name}")
+    print(f"\n===============================================")    
+    exit(1)
